efc9d089a6be92d92a44c6b8a4107e1d774d9d3e
Brightcells <brightcells@gmail.com>
2018-02-28 10:03:07 +0800

    :bug: https://github.com/Brightcells/pyemoji/issues/2

--- pyemoji/main.py
+++ pyemoji/main.py
@@ -9,12 +9,13 @@ from .compat import str
 
 # Refer: http://stackoverflow.com/questions/26568722/remove-unicode-emoji-using-re-in-python
 # Refer: http://stackoverflow.com/questions/22706522/python-remove-ios-emoji-characters-in-a-unicode-str-to-avoid-databaseerror-in
+# Refer: https://stackoverflow.com/questions/13729638/how-can-i-filter-emoji-characters-from-my-input-so-i-can-save-in-mysql-5-5
 try:
     # Wide UCS-4 build
-    highpoints = re.compile('[\U00010000-\U0010ffff]')
+    highpoints = re.compile(u'([\U00002600-\U000027BF])|([\U0001f300-\U0001f64F])|([\U0001f680-\U0001f6FF])')
 except re.error:
     # Narrow UCS-2 build
-    highpoints = re.compile('[\uD800-\uDBFF][\uDC00-\uDFFF]')
+    highpoints = re.compile(u'([\u2600-\u27BF])|([\uD83C][\uDF00-\uDFFF])|([\uD83D][\uDC00-\uDE4F])|([\uD83D][\uDE80-\uDEFF])')
 
 
 class PyEmoji(object):
